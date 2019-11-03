927a8349a5366049b14b724c5044a586aefe5da0
Drumanagh Wilpole <drumanagh@alienspacebats.com>
2019-11-02 20:21:43 -0700

    Reference new typings in custom typings root

--- src/typings/my.d.ts
+++ src/typings/my.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="my/fix.d.ts" />
 /// <reference path="my/canvas.d.ts" />
+/// <reference path="my/git-describe.d.ts" />
 /// <reference path="my/node-async.d.ts" />
 /// <reference path="my/webgl-debug.d.ts" />
